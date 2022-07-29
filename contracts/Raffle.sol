34a166857f518a29120d5e796496917a80e0243f
Borislav Stoyanov <bostoy@protonmail.com>
2022-07-29 22:14:26 +0300

    🚧 Create Raffle Contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,36 @@
+//Raffle
+//Enter the lottery (paying some amount)
+//Pick a random winner (verifiably random)
+//Winner to be selected exery X minuts -> completely automated
+//Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+    // State Variables
+    uint256 private immutable i_entranceFee;
+    address[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+
+        s_players.push(payable(msg.sender));
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
