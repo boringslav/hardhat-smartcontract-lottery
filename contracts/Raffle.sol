6d0b2ad43916d9637f87713c00baa14b2fe90e0d
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-06 17:19:38 +0300

    🚧 Implemented Chainlink VRF (Introduction)
    :qa

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,9 +7,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // State Variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -17,7 +19,7 @@ contract Raffle {
     //Events - Named events with the function name reversed (convention)
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -30,6 +32,18 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {
+        //Request the random number
+        //Once we get the number, do something with it
+        //2 transaction process
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    // View / Pure functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
