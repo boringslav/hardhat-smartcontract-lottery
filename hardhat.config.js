a15e23123a6c866f68ea75ce03ef0b2d5176d57d
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-17 15:16:09 +0300

    🔧 Update Hardhat config / Create Hardhat config helper Add hardhat and rinkeby networks to hardhat config, add the url of the vrfCoordinatorV2

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,34 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { RINKEBY_RPC_URL, PRIVATE_KEY, COINMARKETCAP_API_KEY, ETHERSCAN_API_KEY } = process.env
+
 module.exports = {
-  solidity: "0.8.9",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        rinkeby: {
+            chainId: 4,
+            blockConfirmations: 6,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: "0.8.9",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
