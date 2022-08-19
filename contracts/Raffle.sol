b61b72d88f7fc2035ec35b4624a027e79bd53dc7
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-19 15:18:07 +0300

    ✨ Raffle contract getInterval function

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -174,4 +174,8 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatible {
     function getRequestConfirmations() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
     }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
 }
