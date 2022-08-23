4fd8c6d02559baa2ce008939e31eea5d5a0b3c82
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-23 16:27:38 +0300

    ✨ Frontend script: create a function that updates the frontend abi.json file

--- deploy/99-update-frontend.js
+++ deploy/99-update-frontend.js
@@ -2,12 +2,13 @@ const fs = require("fs")
 const { ethers, network } = require("hardhat")
 
 const FRONT_END_ADDRESSES_FILE = "../nextjs-smartcontract-lottery/constants/contractAddresses.json"
-const FRONT_END_ABI_FILE = "../../nextjs-smartcontract-lottery/constants/abi.json"
+const FRONT_END_ABI_FILE = "../nextjs-smartcontract-lottery/constants/abi.json"
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating frontend...")
         await updateContractAddresses()
+        await updateAbi()
         console.log("Frontend updated!")
     }
 }
@@ -28,4 +29,8 @@ const updateContractAddresses = async () => {
     fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
 }
 
+const updateAbi = async () => {
+    const raffle = await ethers.getContract("Raffle")
+    fs.writeFileSync(FRONT_END_ABI_FILE, raffle.interface.format(ethers.utils.FormatTypes.json))
+}
 module.exports.tags = ["all", "frontend"]
